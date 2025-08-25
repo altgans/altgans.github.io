b40169b94fb42f90ee6ea9b631ed3e180d043280
JS <zilch+git@mailbox.org>
2025-08-25 22:05:21 +0200

    tailwind serve

--- eleventy.config.js
+++
@@ -1,6 +0,0 @@
-import EleventyVitePlugin from "@11ty/eleventy-plugin-vite";
-
-export default function (eleventyConfig) {
-	eleventyConfig.addPlugin(EleventyVitePlugin);
-};
-
---
+++ tailwind.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  content: ["./src/**/*.{html,njk,md}", "./.eleventy.js"],
+  theme: { extend: {} },
+  plugins: [],
+}
